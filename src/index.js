a19270bc35d99cd2e5068d74c85404353965ae05
MrFranke <mmoskalev@ctcmedia.ru>
2018-11-08 00:12:47 +0300

    add swagger-parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
 import { configure } from "./components/configure";
+import { bellhop } from "./components/bellhop";
+import SwaggerParser from "swagger-parser";
 
 const program = configure();
+const specPath = program.spec;
+SwaggerParser.parse(specPath).then(res => {
+  debugger
+});
+
+// bellhop(specPath).then((spec: string) => {
+//
+//
+// });
