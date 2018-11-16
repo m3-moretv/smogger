97735d81c6a3bc7ad9d02e505fc20bb62766c28a
Михаил Москалев <mmoskalev@ctcmedia.ru>
2018-11-16 16:03:44 +0300

    add date type

--- src/components/mocker/index.js
+++ src/components/mocker/index.js
@@ -21,6 +21,7 @@ const formatFakerTypes = (type) => {
 const createFakeData = ({type, format, min = 0, max = 99999999, minLength = 0, maxLength = 100, ...rest}) => {
   if ('enum' in rest) { return randomElement(rest.enum); }
   if ('nullable' in rest && random.boolean()) { return null; }
+  if ('date' in rest && random.boolean()) { return faker.data.between('2015-01-01', '2021-01-01'); }
 
   const normalizeType = formatFakerTypes(type);
   const ftype = format && format.includes('.') ? objectPath(faker, format) : faker.random[normalizeType];
