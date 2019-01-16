1c35e9b7ad04e1d6284bf839573b1d4432981b94
MrFranke <mmoskalev@ctcmedia.ru>
2019-01-17 00:12:27 +0300

    add hasky, lint-stage and prettier for save code clean in repo

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,16 +1,26 @@
 import Koa from 'koa';
-import { createRouter } from "./generate";
-import { compose, formatSwaggerPath } from "../utils";
-import type Application, {Context} from 'koa';
-import type { PathItem } from "openapi3-flowtype-definition";
+import { createRouter } from './generate';
+import { compose, formatSwaggerPath } from '../utils';
+import type Application, { Context } from 'koa';
+import type { PathItem } from 'openapi3-flowtype-definition';
 
 export type Middleware = (path: string, method: string, data?: any) => any;
 
-const dataToResponse: (data: {}, ctx: Context) => string = (data, ctx) => ctx.body = JSON.stringify(data);
-type CreateMiddleware = (router: Application, processors: Array<Middleware>) => Application;
+const dataToResponse: (data: {}, ctx: Context) => string = (data, ctx) =>
+  (ctx.body = JSON.stringify(data));
 
-const exposeRequestProps: (ctx: Context) => {path: string, method: string} = ctx => {
-  const {req: {method}, _matchedRoute} = ctx;
+type CreateMiddleware = (
+  router: Application,
+  processors: Array<Middleware>
+) => Application;
+
+const exposeRequestProps: (
+  ctx: Context
+) => { path: string, method: string } = ctx => {
+  const {
+    req: { method },
+    _matchedRoute
+  } = ctx;
   const path = formatSwaggerPath(_matchedRoute);
   return {
     path,
@@ -18,22 +28,28 @@ const exposeRequestProps: (ctx: Context) => {path: string, method: string} = ctx
   };
 };
 
+export const createMiddleware: CreateMiddleware = (
+  app: Application,
+  middlewares: Middleware[]
+) =>
+  app.use((ctx, next) => {
+    let data = {};
+    if (!ctx.matched.length) {
+      return next();
+    }
+    const { path, method } = exposeRequestProps(ctx);
+    const processor = compose(...middlewares);
+    data = processor(path, method, data);
+    dataToResponse(data, ctx);
+  });
 
-export const createMiddleware: CreateMiddleware = (app: Application, middlewares: Middleware[]) => app.use((ctx, next) => {
-  let data = {};
-  if (!ctx.matched.length) { return next() }
-  const {path, method} = exposeRequestProps(ctx);
-  const processor = compose(...middlewares);
-  data = processor(path, method, data);
-  dataToResponse(data, ctx);
-});
-
-export const createHTTPServer = ({ port }: {port: number}, middlewares: Middleware[]) => (paths: PathItem): Application => {
+export const createHTTPServer = (
+  { port }: { port: number },
+  middlewares: Middleware[]
+) => (paths: PathItem): Application => {
   const app = new Koa();
   const router = createRouter(paths);
-  app
-    .use(router.routes())
-    .use(router.allowedMethods());
+  app.use(router.routes()).use(router.allowedMethods());
 
   createMiddleware(app, middlewares);
 
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,16 +1,45 @@
 import random from 'random';
 
-export const entries: (obj: {}) => Array<[string, any]> = Object.entries.bind(Object);
-export const run: (Function) => any = fn => fn();
-export const normalizer: (Function) => (Array<any>, Array<any>) => Array<any> = handler => (acc, arr) => acc.concat(handler(arr));
-export const spreadToArgs: (cb: Function) => (obj: {} | Array<any>) => any = cb => props => cb(...props);
-export const normalizeUrlParams = (subst: RegExp, brackets?: string = ':$1') => (path: string) => path.replace(subst, brackets);
-export const formatSwaggerPath: (path: string) => string = normalizeUrlParams(/:(\w+)/g, '{$1}');
-export const formatRouterPath: (path: string) => string = normalizeUrlParams(/{(\w+)}/g, ':$1');
-export const objectPath: (object: {}, path: string) => any = (object, path) => path.split('.').reduce((acc, part) => acc[part], object);
-export const randomElement: (arr: Array<any>) => any = arr => arr[random.int(0, arr.length-1)];
-export const refToObjectPath: (ref: string) => string = ref => ref.slice(2).replace('/', '.');
-export const compose = (...fns: any) => fns.reduce((f, g) => (...xs) => {
-  const r = g(...xs);
-  return Array.isArray(r) ? f(...r) : f(r);
-});
+export const entries: (obj: {}) => Array<[string, any]> = Object.entries.bind(
+  Object
+);
+export const run: Function => any = fn => fn();
+
+export const normalizer: Function => (
+  Array<any>,
+  Array<any>
+) => Array<any> = handler => (acc, arr) => acc.concat(handler(arr));
+
+export const spreadToArgs: (
+  cb: Function
+) => (obj: {} | Array<any>) => any = cb => props => cb(...props);
+
+export const normalizeUrlParams = (
+  subst: RegExp,
+  brackets?: string = ':$1'
+) => (path: string) => path.replace(subst, brackets);
+
+export const formatSwaggerPath: (path: string) => string = normalizeUrlParams(
+  /:(\w+)/g,
+  '{$1}'
+);
+
+export const formatRouterPath: (path: string) => string = normalizeUrlParams(
+  /{(\w+)}/g,
+  ':$1'
+);
+
+export const objectPath: (object: {}, path: string) => any = (object, path) =>
+  path.split('.').reduce((acc, part) => acc[part], object);
+
+export const randomElement: (arr: Array<any>) => any = arr =>
+  arr[random.int(0, arr.length - 1)];
+
+export const refToObjectPath: (ref: string) => string = ref =>
+  ref.slice(2).replace('/', '.');
+
+export const compose = (...fns: any) =>
+  fns.reduce((f, g) => (...xs) => {
+    const r = g(...xs);
+    return Array.isArray(r) ? f(...r) : f(r);
+  });
